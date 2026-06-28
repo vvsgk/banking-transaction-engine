33d041cb2af1771ce98c8f8f776f71b72940974f
Ganesh Kumar Vadlavalli <vadlavalli@icloud.com>
2026-06-28 22:45:27 +0530

    Version 0.0.1 - Test Git upload endpoints

---
+++ src/main/java/com/vvsgk/banking_transaction_engine/BankingTransactionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.vvsgk.banking_transaction_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingTransactionEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingTransactionEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vvsgk/banking_transaction_engine/BankingTransactionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvsgk.banking_transaction_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTransactionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
